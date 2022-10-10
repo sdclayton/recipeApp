6bc94929fd5915feb835d9d387d9791982d61bd0
sdclayton <96846005+sdclayton@users.noreply.github.com>
2022-10-10 18:59:50 -0500

    Add personal project...recipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EditShoppingListComponent } from './shopping-list/edit-shopping-list/edit-shopping-list.component';
+import { ListComponent } from './recipes/list/list.component';
+import { ItemsComponent } from './recipes/list/items/items.component';
+import { DetailsComponent } from './recipes/details/details.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipesComponent } from './recipes/recipes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditShoppingListComponent,
+    ListComponent,
+    ItemsComponent,
+    DetailsComponent,
+    NavbarComponent,
+    ShoppingListComponent,
+    RecipesComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,20 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+  @Output() featureSelected = new EventEmitter<string>();
+
+  onSelect(feature: string) {
+    this.featureSelected.emit(feature);
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
